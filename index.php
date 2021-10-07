e34ecfa44710e5ff1463a2e58bfb3ea117ce306e
gabiarzu <gabiarzu@gmail.com>
2021-10-07 12:47:14 +0200

    first index change

--- index.php
+++ index.php
@@ -5,8 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project 1</title>
+    <style>
+
+        body {
+            background: #000;
+            color: #fff;
+        }
+        div {
+            margin: 100px auto;
+        }
+
+    </style>
 </head>
 <body>
-    
+    <div>
+
+        <h1>Project 1 Index</h1>
+
+        <p>Good Luck!</p>
+
+    </div>
 </body>
 </html>
